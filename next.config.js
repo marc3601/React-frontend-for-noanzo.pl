7f132d0f1b603c471f6e852cdefb8bb8887a2487
Marcin <marcin.czaja2@gmail.com>
2021-06-22 21:28:32 +0200

    initial

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["ireland.apollo.olxcdn.com", "picsum.photos"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  }
+
 }
