ecd4fb78ff3d2f888c31ec54c846b969b8cf4cbd
Marcin <marcin.czaja2@gmail.com>
2021-06-29 23:10:33 +0200

    init firebase

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["ireland.apollo.olxcdn.com", "picsum.photos"],
+    domains: ["firebasestorage.googleapis.com", "ireland.apollo.olxcdn.com", "picsum.photos", "unsplash.com"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   }
