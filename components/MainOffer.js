f74238fbb38deed539f348fedbb0fa808d71184f
Marcin <marcin.czaja2@gmail.com>
2021-06-30 22:18:05 +0200

    ISR fix attempt 1

--- components/MainOffer.js
+++ components/MainOffer.js
@@ -6,16 +6,16 @@ const MainOffer = ({ post }) => {
     return (
         <section className={styles.top_listing_container}>
             <div className={styles.top_description_container}>
-                <h1 className={styles.main_title}>{post[0].title}</h1>
-                <p className={styles.main_description}>
+                {post && <h1 className={styles.main_title}>{post[0].title}</h1>}
+                {post && <p className={styles.main_description}>
                     {post[0].desc}
-                </p>
+                </p>}
                 <button className={styles.main_button}>
                     <a href="tel:+48513904044">Kontakt</a>
                 </button>
             </div>
             <div className={styles.top_image_container}>
-                <Image className={styles.main_image} alt="testy" width={850} height={450} src={post[0].url} />
+                {post && <Image className={styles.main_image} alt="testy" width={850} height={450} src={post[0].url} />}
             </div>
         </section>
     )
