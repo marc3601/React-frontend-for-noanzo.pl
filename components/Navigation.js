3e1941f5f2c4f8ea9aebbdb7af19dabb388382e0
Marcin <marcin.czaja2@gmail.com>
2021-07-20 23:42:31 +0200

    tags added

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,17 +4,24 @@ import Loupe from '../utilities/Loupe'
 import styles from "../styles/Navigation.module.css"
 const Navigation = () => {
     return (
-        <div className={styles.navigation_section}>
-            <Link href="/">
-                <div className={styles.logo}>ANZO</div>
-            </Link>
-            <div className={styles.search_container}>
-                <div className={styles.search_wrapper}>
-                    <Loupe />
-                    <input className={styles.search_input} placeholder="Szukaj" />
+        <>
+            <div className={styles.navigation_section}>
+                <Link href="/">
+                    <div className={styles.logo}>ANZO</div>
+                </Link>
+                <div className={styles.search_container}>
+                    <div className={styles.search_wrapper}>
+                        <Loupe />
+                        <input className={styles.search_input} placeholder="Szukaj" />
+                    </div>
+
                 </div>
             </div>
-        </div>
+            <div className={styles.tag_wrapper}>
+                <span className={styles.tag}>#budydlapsow</span>
+                <span className={styles.tag}>#budydlakotow</span>
+            </div>
+        </>
     )
 }
 
