d3075ab5367f2a09b31124d71c9fe20dbf840469
Marcin <marcin.czaja2@gmail.com>
2021-09-17 20:55:53 +0200

    Gif in frontend added.

--- components/Gallery.js
+++ components/Gallery.js
@@ -12,17 +12,22 @@ const Gallery = ({ images, loading }) => {
                     !loading && images?.map((auction, i) => {
                         let height = auction.image[0]?.height
                         let width = auction.image[0]?.width
-                        let link = null
+                        let link = auction.image[0]?.url
                         const thumb = auction.image?.filter(item => item.thumbnail === true)
                         if (thumb.length > 0) {
                             link = thumb[0]?.url
                             height = thumb[0]?.height;
                             width = thumb[0]?.width;
                         }
+                        if (auction.gif !== undefined && auction.gif.url.length > 0 && auction.gif.width > 0) {
+                            link = auction.gif.url;
+                            width = auction.gif.width;
+                            height = auction.gif.height;
+                        }
                         return <Link key={auction.id + i} href={"/" + auction.id}>
                             <div className={`${styles.card} ${height < 300 ? styles.card_small : height >= 300 && height < 600 ? styles.card_medium : styles.card_large}`} >
-                                <Image className={styles.image} unoptimized={true} objectFit="contain" layout="intrinsic" src={link === null ? auction.image[0]?.url : link} alt={auction.title} width={width} height={height} />
-                                <aside className={styles.image_title}><p>{auction.title}</p></aside>
+                                <Image className={styles.image} unoptimized={true} objectFit="contain" layout="intrinsic" src={link} alt={auction.title} width={width} height={height} />
+                                {height > 300 && <aside className={styles.image_title}><p>{auction.title}</p></aside>}
                             </div>
                         </Link>
                     })
