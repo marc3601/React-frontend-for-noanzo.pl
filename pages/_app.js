69ea7635d46381babb6c359e08953b8b0d569e89
Marcin <marcin.czaja2@gmail.com>
2021-09-27 21:04:57 +0200

    Analitics aded

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import Router from "next/router"
 import { useState } from "react"
+import { useEffect } from 'react'
 import TopBarProgress from "react-topbar-progress-indicator"
 import '../styles/globals.css'
 import 'swiper/swiper.scss';
+import * as ga from '../lib/ga'
 
 TopBarProgress.config({
   barColors: {
@@ -13,8 +15,24 @@ TopBarProgress.config({
   barThickness: 4
 });
 
+
 function MyApp({ Component, pageProps }) {
   const [progress, setProgress] = useState(false)
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    Router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [Router.events])
 
   Router.events.on("routeChangeStart", () => {
     setProgress(true)
